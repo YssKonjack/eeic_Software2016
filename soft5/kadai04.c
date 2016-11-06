47c30310138fa7431c15a6e9150e60536b6d56ea
U-YShimomichi-PC\Y.Shimomichi <moccolipocoli@yahoo.co.jp>
2016-11-06 21:36:25 +0900

    kadai04

--- soft5/kadai04.c
+++ soft5/kadai04.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<unistd.h>
 #define HEIGHT 15
 #define WIDTH  30
 #define TEXT_SIZE 2*HEIGHT*WIDTH+1 //最大で200%
@@ -12,26 +11,21 @@ void file_deployment(char matrix[HEIGHT][WIDTH],char string[TEXT_SIZE]);
 void drawing(char matrix[HEIGHT][WIDTH]);
 void matrix_update(char matrix[HEIGHT][WIDTH]);
 
-
 int main(){
   char matrix[HEIGHT][WIDTH];
   FILE *fp;
   fp = fopen("data.txt","w");
   fclose(fp);//初期化
-  char s[TEXT_SIZE]; 
+  char str[TEXT_SIZE];
 
   initialize(matrix);
   runlength_save(fp,matrix);
- 
   while(1){
     system("clear");
-
-    file_read(fp,s);
-    file_deployment(matrix,s);     
+    file_read(fp,str);
+    file_deployment(matrix,str);
     drawing(matrix);
-
     matrix_update(matrix);
-    
     runlength_save(fp,matrix);
   }
   return 0;
@@ -42,44 +36,45 @@ void initialize(char matrix[HEIGHT][WIDTH]){
   int i,j;
   for(i = 0;i < HEIGHT;i++){
     for(j = 0;j < WIDTH;j++){
+      if(j%10==0){
+        matrix[i][j] = '*';
+      }else{
       matrix[i][j] = '.';
+      }
     }
   }
 }
 
 
-void runlength_save(FILE *fp,char matrix[HEIGHT][WIDTH]){  //その１ 
+void runlength_save(FILE *fp,char matrix[HEIGHT][WIDTH]){  //その１
   int i,j;
   char w = matrix[0][0]; //直前の文字
-  char int_char[] = "";
+  char int_char[1] = " ";
   int cnt = 0;
-  
+
   fp = fopen("data.txt","w");
   for(i = 0; i < HEIGHT; i++){
     for(j = 0; j < WIDTH; j++){
       if(w == matrix[i][j]){
-	cnt++;     //直前の文字と同じだったら
+       	cnt++;       //直前の文字と同じだったら
       }else{
 	sprintf(int_char,"%d",cnt);
 	fprintf(fp,"%c%c",w,int_char[0]);
-	
+
 	w = matrix[i][j];
 	cnt = 1;
       }
     }
   }
-  sprintf(int_char,"%d",cnt);
-  fprintf(fp,"%c%c",w,int_char[0]);
-  fclose(fp);
+ fclose(fp);
 }
-
 
 void file_read(FILE *fp,char str[TEXT_SIZE]){
   if((fp = fopen("data.txt","r")) == NULL){     //その２ 課題３のまま
     printf("ファイルが見つかりません");
     exit(EXIT_FAILURE);
-  }    
-  fgets(str,TEXT_SIZE,fp);   
+  }
+  fgets(str,TEXT_SIZE,fp);
   fclose(fp);
 }
 
@@ -90,19 +85,18 @@ void file_deployment(char matrix[HEIGHT][WIDTH],char str[TEXT_SIZE]){
   int rep = 0;//繰り返し回数(最大9回)
   int cnt = 0;//展開位置
   char a[] = " ";//現在の文字
-  
+
   for(i = 0; i < HEIGHT; i++){
     for(j = 0; j < WIDTH; j++){
       if(rep == 0||rep > 9){
 	a[0] = str[cnt];
-	rep = atoi(&str[cnt+1]);
+	rep = atoi(&str[++cnt]);
 	cnt++;
- 
+
 	matrix[i][j] = a[0];
 	rep--;
       }else{
 	matrix[i][j] = a[0];
-	cnt++;
 	rep--;
       }
     }
